93aafdb238107ec42e71bb354b4176f7f7e770be
DESKTOP-60RKR9O\neato <neatouo@naver.com>
2022-04-22 00:52:28 +0900

    test : CarName 테스트 자동차의 이름의 길이가 조건에 맞는다면 정상적으로 객체를 생성하는지 확인한다. 자동차의 이름의 길이가 조건에 맞지않는다면, IllegalArgumentException에러를 발생 시키는지 확인한다.

---
+++ src/test/java/racingcar/domain/car/component/CarNameTest.java
@@ -0,0 +1,26 @@
+package racingcar.domain.car.component;
+
+import org.junit.jupiter.api.Test;
+import racingcar.validator.name.NextStepValidatorName;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarNameTest {
+
+    @Test
+    void 이름_정상_반환_테스트() {
+        CarName carName = new CarName("test", new NextStepValidatorName());
+        assertEquals(carName.getName(), "test", "'test'를 정상적으로 반환해야 합니다");
+    }
+
+    @Test
+    void 이름_정상_검증_테스트() {
+        assertThatThrownBy(
+                () -> {
+                    CarName carName = new CarName("testtest", new NextStepValidatorName());
+                }
+        ).isInstanceOf(IllegalArgumentException.class);
+
+    }
+}
