72792d2c71b3181cc142f5e6705faa19825e3bb1
DESKTOP-60RKR9O\neato <neatouo@naver.com>
2022-04-23 12:05:35 +0900

    test : CarFactory, NextStepCarFactory test 자동차 갯수가 0일때 에러 발생하는지 확인 각 객체에 맞는 객체(car or Nextstepcar)를 반환하는지 확인 carFactory는 engine과 validatorName이 설정안되있을 때 에러 반환하는지 확인

---
+++ src/test/java/racingcar/domain/factory/CarFactoryTest.java
@@ -0,0 +1,62 @@
+package racingcar.domain.factory;
+
+import org.junit.jupiter.api.Test;
+import racingcar.domain.car.Car;
+import racingcar.domain.cars.Cars;
+import racingcar.domain.engine.NextStepEngine;
+import racingcar.generator.NextStepFuelGenerator;
+import racingcar.validator.name.NextStepValidatorName;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarFactoryTest {
+
+    @Test
+    void 자동차_갯수_0_에러_확인() {
+        assertThatThrownBy(
+                () -> new CarFactory()
+                        .setEngine(new NextStepEngine(
+                                new NextStepFuelGenerator()
+                        ))
+                        .setValidatorName(new NextStepValidatorName())
+                        .makeCar("")
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void ENGINE_NULL_에러_확인() {
+        assertThatThrownBy(
+                () -> new CarFactory()
+                        .setValidatorName(new NextStepValidatorName())
+                        .makeCar("car1,car2")
+        ).isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void VALIDATORNAME_에러_확인() {
+        assertThatThrownBy(
+                () -> new CarFactory()
+                        .setEngine(new NextStepEngine(
+                                new NextStepFuelGenerator()
+                        ))
+                        .makeCar("car1,car2")
+        ).isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void 리스트_정상_반환_확인() {
+        Cars cars = new CarFactory()
+                .setEngine(new NextStepEngine(
+                        new NextStepFuelGenerator()
+                ))
+                .setValidatorName(new NextStepValidatorName())
+                .makeCar("car1,car2");
+
+        assertEquals("car1", cars.getCars().get(0).getName(),"설정된 첫번째 자동차는 'car1'이여야 합니다.");
+        assertEquals("car2", cars.getCars().get(1).getName(),"설정된 첫번째 자동차는 'car2'이여야 합니다.");
+
+        assertNotNull(cars.getCars().get(0), "설정된 자동차는 car 객체여야 합니다.");
+        assertNotNull(cars.getCars().get(1), "설정된 자동차는 car 객체여야 합니다.");
+    }
+}
---
+++ src/test/java/racingcar/domain/factory/NextStepCarFactoryTest.java
@@ -0,0 +1,33 @@
+package racingcar.domain.factory;
+
+import org.junit.jupiter.api.Test;
+import racingcar.domain.car.NextStepCar;
+import racingcar.domain.cars.Cars;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class NextStepCarFactoryTest {
+
+    @Test
+    void 자동차_갯수_0_에러_확인() {
+        assertThatThrownBy(
+                () -> new NextStepCarFactory()
+                        .makeCar("")
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 리스트_정상_반환_확인() {
+        Cars cars = new NextStepCarFactory()
+                .makeCar("car1,car2");
+
+        assertEquals("car1", cars.getCars().get(0).getName(),"설정된 첫번째 자동차는 'car1'이여야 합니다.");
+        assertEquals("car2", cars.getCars().get(1).getName(),"설정된 첫번째 자동차는 'car2'이여야 합니다.");
+
+        assertTrue(cars.getCars().get(0) instanceof NextStepCar,"설정된 자동차는 NextStepCar 객체여야 합니다.");
+        assertTrue(cars.getCars().get(1) instanceof NextStepCar,"설정된 자동차는 NextStepCar 객체여야 합니다.");
+
+    }
+
+}
