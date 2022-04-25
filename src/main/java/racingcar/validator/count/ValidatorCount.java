1bc1ee1f17d6ffe6fc2daa653cc052a9b1ac4afe
DESKTOP-60RKR9O\neato <neatouo@naver.com>
2022-04-25 15:13:54 +0900

    feat : user input count가 0 이상인지 확인하도록 수정

--- src/main/java/racingcar/validator/count/ValidatorCount.java
+++ src/main/java/racingcar/validator/count/ValidatorCount.java
@@ -6,12 +6,22 @@ import racingcar.config.Message;
 public class ValidatorCount {
     public int validateCnt() throws IllegalArgumentException{
         int cnt;
+
         try{
             cnt=  Integer.parseInt(Console.readLine());
-            System.out.println(cnt);
-        } catch (Exception e) {
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException(Message.ERROR_COUNT_WRONG);
         }
+
+        checkUnderZero(cnt);
+
+        System.out.println(cnt);
         return cnt;
+    }
+
+    public void checkUnderZero(int cnt) throws IllegalArgumentException{
+        if(cnt < 1){
+            throw new IllegalArgumentException(Message.ERROR_INVALID_CNT);
+        }
     }
 }
