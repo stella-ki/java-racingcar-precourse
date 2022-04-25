2857ac858781b123a5c64692b68630a594f6f1cf
DESKTOP-60RKR9O\neato <neatouo@naver.com>
2022-04-25 10:43:30 +0900

    refactor : NextStepEngine 에러 문구 추가 범위에 벗어난 랜덤값이 input시 에러 문구 표시 할 수 있도록 추가

--- src/main/java/racingcar/domain/engine/NextStepEngine.java
+++ src/main/java/racingcar/domain/engine/NextStepEngine.java
@@ -11,7 +11,7 @@ public class NextStepEngine extends Engine{
 
     public boolean gearShift(int value){
         if(value > Config.MAX_FUAL_VALUE || value < Config.MIN_INPUT_VALUE){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("랜덤값의 범위를 벗어났습니다.");
         }
         return value >= Config.MOVE_FORWARD_FUAL_VALUE;
     }
