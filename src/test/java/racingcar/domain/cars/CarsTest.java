3efd07844dc83911ee4a6e2fa132181758f5d6dd
DESKTOP-60RKR9O\neato <neatouo@naver.com>
2022-04-25 13:25:03 +0900

    test : car 정렬 테스트 보강

--- src/test/java/racingcar/domain/cars/CarsTest.java
+++ src/test/java/racingcar/domain/cars/CarsTest.java
@@ -69,4 +69,41 @@ class CarsTest {
                 1,new Integer[]{5,3,3,7,2}
         );
     }
+
+
+    @Test
+    @DisplayName("Cars 객체가 location과 name 기준으로 정상적으로 정렬하는지 확인한다.")
+    void 자동차_정렬_테스트() {
+
+        assertRandomTest(
+                () -> Randoms.pickNumberInRange(anyInt(), anyInt()),
+                () -> {
+
+                    List<Car> carList = new ArrayList<>();
+
+                    NextStepCar nextStepCar1 = new NextStepCar("car1");
+                    NextStepCar nextStepCar2 = new NextStepCar("car2");
+                    NextStepCar nextStepCar3 = new NextStepCar("car3");
+
+                    for(int i=0; i<2; i++){
+                        nextStepCar1.move();
+                        nextStepCar2.move();
+                        nextStepCar3.move();
+                    }
+
+                    carList.add(nextStepCar1);
+                    carList.add(nextStepCar2);
+                    carList.add(nextStepCar3);
+
+                    Cars cars = new Cars(carList);
+                    cars.getMaxLocation();
+
+                    assertEquals("car2", cars.getCar(0).getName(), "정렬 후 첫번째 car는 'car2'여야 합니다.");
+                    assertEquals("car3", cars.getCar(1).getName(), "정렬 후 두번째 car는 'car3'여야 합니다.");
+                    assertEquals("car1", cars.getCar(2).getName(), "정렬 후 세번째 car는 'car1'여야 합니다.");
+
+                },
+                1,new Integer[]{5,7,3,7,8}
+        );
+    }
 }
