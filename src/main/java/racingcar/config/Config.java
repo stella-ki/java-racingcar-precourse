cbd21d40ee4b6d6d6dd4772fd3d4776a115336d8
DESKTOP-60RKR9O\neato <neatouo@naver.com>
2022-04-25 10:44:16 +0900

    refactor : 조건값 config에 추가 max값과 전진 조건값 추가

--- src/main/java/racingcar/config/Config.java
+++ src/main/java/racingcar/config/Config.java
@@ -4,5 +4,8 @@ public class Config {
     public static final int MIN_INPUT_VALUE = 0;
     public static final int MAX_INPUT_VALUE = 9;
 
+    public static final int MAX_FUAL_VALUE = 9;
+    public static final int MOVE_FORWARD_FUAL_VALUE = 4;
+
     public static final int MAX_NAME_LENGTH = 5;
 }
